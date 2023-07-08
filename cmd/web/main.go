eee7ab4d51b7624ab1462ba8a1191433b1978945
Peter DADA <dapetoo@gmail.com>
2023-07-08 13:39:22 +0100

    Refactoring to use flag and custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	//This will be used when running the app from the command line
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	//
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Llongfile)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Starting the webserver on port :4000")
-	err := http.ListenAndServe(":4000", mux)
-	if err != nil {
-		log.Fatal(err)
+	//FileServer to serve static files
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	//Register the File server with mux.Handle
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	infoLog.Printf("Starting the webserver on port %v", *addr)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
 	}
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
