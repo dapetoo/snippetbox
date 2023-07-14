a68ff87faf37341674aa189e73e4eb9bec6b447c
Peter DADA <dapetoo@gmail.com>
2023-07-14 22:10:35 +0100

    Refactoring and using Chi Router for Routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,16 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"github.com/dapetoo/snippetbox/pkg/models"
+	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -27,7 +24,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -47,6 +44,34 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "show.page.tmpl", &templateData{
 		Snippet: s,
 	})
+}
+
+func (app *application) showSnippet2(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	snippetID, err := strconv.Atoi(id)
+	if err != nil || snippetID < 1 {
+		app.notFound(w)
+		return
+	}
+
+	// Use the Snippet object's Get method to retrieve the data for a specific record based on its ID
+	s, err := app.snippets.GetByID(snippetID)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/justinas/alice"
 	"net/http"
 )
@@ -8,15 +9,18 @@ import (
 func (app *application) routes() http.Handler {
 
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	//Using Chi as a Router
+	mux := chi.NewRouter()
+	mux.Get("/", app.home)
+	mux.Get("/snippet/create", app.createSnippet)
+	mux.Post("/snippet/create", app.createSnippet)
+	mux.Get("/snippet/{id}", app.showSnippet2)
 
 	//FileServer to serve static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	//Register the File server with mux.Handle
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return standardMiddleware.Then(mux)
 }
