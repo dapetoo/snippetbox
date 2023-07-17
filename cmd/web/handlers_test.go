6b856fa23e25ded246d2bd852ef8c7b52145fc03
Peter DADA <dapetoo@gmail.com>
2023-07-17 15:22:38 +0100

    Ping Handler test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func Test_ping(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+func TestPing(t *testing.T) {
+	//Initialize a new httptest.ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	//Initialize a new dummy http.Request
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
 	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
+
+	//Call the ping handler function, passing in the ResponseRecorder and the Request
+	ping(rr, r)
+
+	//Call the Result() on the ResponseRecorder to get the http.Response generated
+	rs := rr.Result()
+
+	//Examine the response to check that the status code written by the Ping Handler was 200
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ping(tt.args.w, tt.args.r)
-		})
+
+	//Check the Response body written by the ping handler equals "OK"
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q got %q", "OK", body)
 	}
 }
